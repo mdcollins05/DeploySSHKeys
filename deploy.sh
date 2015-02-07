d2162d902156ec3d05885d0e74305b45cadd5b6b
Matt Collins <mcollins@pagerduty.com>
2015-02-06 17:56:40 -0800

    Added some more notes on possible commands to detect if a user exists on a system.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ FILES=./keys/*.key
 for file in $FILES
 do
   # if ~user exists, create .ssh, move original authorized_keys, copy user.key to authorized_keys
+  # id -u matt > /dev/null 2>&1 ; echo $?
 done
