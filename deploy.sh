5bca08e92904f4466d579e91402cb4a83c104fe7
MDCollins <matt@m-collins.com>
2015-02-08 18:00:09 -0800

    Added skipping key files unless the key file is for the current user _OR_ running as root.

--- deploy.sh
+++ deploy.sh
@@ -20,36 +20,42 @@ for keyfile in $FILES
 do
   filename=$(basename "$keyfile")
   user=${filename%.*}
-  # if ~user exists, create .ssh, move original authorized_keys, copy user.key to authorized_keys
-  # id -u matt > /dev/null 2>&1 ; echo $?
 
-  if [ -z "$user" ]
+  if [ -z "$user" ] # Make sure we have something
   then
-    continue
+    continue # Variable is empty, let's skip this one
   fi
 
-  if [ `id -u ${user} > /dev/null 2>&1` ]
+  if [ "$EUID" -ne 0 ] # Are we running as root?
   then
-    continue
-  fi
-
-  userhome=`getent passwd ${user} | cut -f6 -d:` # This should probably change, but maybe we can fix it above.
-
-  if [ ! -d "${userhome}/.ssh/" ]
-  then
-    mkdir ${userhome}/.ssh/
-    chmod 700 ${userhome}/.ssh/
-  fi
-
-  if [ -f "${userhome}/.ssh/authorized_keys" ]
-  then
-    if [ ! `diff "${keyfile}" "${userhome}/.ssh/authorized_keys" > /dev/null 2>&1` ]
+    if [ `id -u ${user}` -ne "${EUID}" ] # Are we handling the keys file for the current user?
     then
-      mv "${userhome}/.ssh/authorized_keys" "${userhome}/.ssh/authorized_keys.orig-$(date +%Y%m%d-%H%M%S)"
+      continue # Nope, let's skip this one
     fi
   fi
 
-  cp ${keyfile} ${userhome}/.ssh/authorized_keys
-  chmod 600 ${userhome}/.ssh/authorized_keys
+  if [ `id -u ${user} > /dev/null 2>&1` ] # Is the user on the system?
+  then
+    continue # User doesn't exist, skip!
+  fi
+
+  userhome=`getent passwd ${user} | cut -f6 -d:` # Let's get the user's home dir!
+
+  if [ ! -d "${userhome}/.ssh/" ] # Does the .ssh folder exist?
+  then
+    mkdir ${userhome}/.ssh/ # Create the folder
+    chmod 700 ${userhome}/.ssh/ # Set those permissions
+  fi
+
+  if [ -f "${userhome}/.ssh/authorized_keys" ] # Does the file exist already?
+  then
+    if [ ! `diff "${keyfile}" "${userhome}/.ssh/authorized_keys" > /dev/null 2>&1` ] # Is there a difference between the two?
+    then
+      mv "${userhome}/.ssh/authorized_keys" "${userhome}/.ssh/authorized_keys.orig-$(date +%Y%m%d-%H%M%S)" # Backup the original
+    fi
+  fi
+
+  cp ${keyfile} ${userhome}/.ssh/authorized_keys # Copy the new contents in place
+  chmod 600 ${userhome}/.ssh/authorized_keys # Set the permissions right
 done
 
